7a486233394ac313c3fb0fcacf00120d64893854
XiaoyanAmy <xiaoyan.dharma@gmail.com>
2023-09-06 13:38:09 +0200

    Update tuning.py

--- src/tuning.py
+++ src/tuning.py
@@ -27,8 +27,6 @@ def model_tuning(model, tuning_method):
         #   prompt tuning
         peft_type = PeftType.PROMPT_TUNING
         peft_config = PromptTuningConfig(task_type="FEATURE_EXTRACTION", 
-                                        #  token_dim=768, 
-                                        #  num_attention_heads = 2, num_layers=2, 
                                         num_virtual_tokens=num_virtual_tokens,
                                         #  prompt_tuning_init="TEXT",
                                         #  prompt_tuning_init_text="Predict if sentiment of this review is positive, negative or neutral"
@@ -37,9 +35,6 @@ def model_tuning(model, tuning_method):
         # prefix tuning
         peft_type = PeftType.PREFIX_TUNING
         peft_config = PrefixTuningConfig(task_type="FEATURE_EXTRACTION", 
-                                        #  num_layers = 12,
-                                        #  token_dim = 768,
-                                        #  num_attention_heads = 12,
                                          num_virtual_tokens=num_virtual_tokens)  
     model_peft = get_peft_model(model, peft_config)
     model_peft.print_trainable_parameters()
